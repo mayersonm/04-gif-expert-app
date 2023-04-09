ab1f53c5ba8eef1ef2b166110306647676df7957
mayerson <mayersonm@gmail.com>
2023-04-09 16:34:24 -0500

    GifsAppReact

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { AddNewCategory , GifGrid } from './components';
+
+export const GifExperApp = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    const onAddCategory = ( newCategory ) => {
+
+        if ( categories.includes( newCategory ) ) return;
+        
+          setCategories([ newCategory, ...categories]);
+
+    }
+
+    return (
+        <>
+            <h1>GifExperApp</h1>
+            <AddNewCategory 
+                onNewCategory={ (value)=> onAddCategory(value) }
+            />
+
+            { 
+                categories.map( (category) => ( 
+                    <GifGrid 
+                        key={category}
+                        category={category}
+                    />
+                )) 
+            }
+
+        </> 
+    )
+}
